412827aa2c3ee1633ecd43840cbb49045637ccdd
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2023-03-15 14:20:07 -0300

    Header & Off Canvas Menu

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+// Get the menu toggle button
+const menuToggle = document.querySelector(".menu-toggle");
+const offCanvas = document.querySelector(".off-canvas");
+const overlay = document.querySelector(".overlay");
+
+menuToggle.addEventListener("click", () => {
+  offCanvas.classList.toggle("show");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  offCanvas.classList.toggle("show");
+  overlay.classList.toggle("show");
+});
+
+const menuClickable = document.querySelectorAll(".menu-clickable");
+const menuShowable = document.querySelectorAll(".menu-showable");
+
+menuClickable.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    menuShowable[index].classList.toggle("show");
+    item.classList.toggle("active");
+  });
+});
